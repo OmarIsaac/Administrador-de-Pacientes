4f7c1c85b68a121bf56fb747c498397738f69287
Isaac Rodriguez <isaac.romo.1002@gmail.com>
2021-03-12 14:09:02 -0600

    Mensaje si no hay citas

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ function App() {
     const nuevascitas = citas.filter((cita) => cita.id !== id);
     guardarCitas(nuevascitas);
   };
+
+  // Mensaje condicional
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus Citas";
   return (
     <Fragment>
       <h1>Administrador de pacientes</h1>
@@ -25,7 +28,7 @@ function App() {
             <Formulario crearCita={crearCita} />
           </div>
           <div className="one-half column">
-            <h2>Administra tus citas</h2>
+            <h2>{titulo}</h2>
             {citas.map((cita) => (
               <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
             ))}
